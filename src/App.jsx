ce9fceb8a38827989c5b3d5cc8cdc6b9f529ddee
KieshaWalker <llcwalkerk@gmail.com>
2025-09-16 15:45:32 -0500

    i think this is what i was supposed to do ?

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,74 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-// src/App.jsx
+
+import './components/WeatherForecast/WeatherForecast.css';
 
 const App = () => {
 
-  return (
-    <h1>Hello world!</h1>
-  );
-}
+  const weatherForecasts = [
+  {
+    day: 'Mon',
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/day.svg',
+    imgAlt: 'sun icon',
+    conditions: 'sunny',
+    time: 'Morning',
+  },
+  {
+    day: 'Tue',
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/night.svg',
+    imgAlt: 'moon icon',
+    conditions: 'clear',
+    time: 'Night',
+  },
+  {
+    day: 'Wed',
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/stormy.svg',
+    imgAlt: 'clouds with lightning icon',
+    conditions: 'stormy',
+    time: 'All Day',
+  },
+  {
+    day: 'Thu',
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-day.svg',
+    imgAlt: 'sun overcast by clouds icon',
+    conditions: 'overcast',
+    time: 'Evening',
+  },
+  {
+    day: 'Fri',
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-night.svg',
+    imgAlt: 'moon overcast by clouds icon',
+    conditions: 'cloudy',
+    time: 'Night',
+  },];
 
-export default App
+  const WeatherIcon = ({ img, imgAlt }) => (
+    <img src={img} alt={imgAlt} />
+  );
+
+  const WeatherData = ({ day, conditions, time }) => (
+    <div>
+      <h2>{day}</h2>
+      <p>{conditions}</p>
+      <p>{time}</p>
+    </div>
+  );
+
+  return (
+    <div className='weather'>
+      <h1>Local Weather</h1>
+      <section>
+        {weatherForecasts.map((forecast) => (
+          <>
+          <span className='weather-icon'><WeatherIcon {...forecast} /></span>
+          <span className='weather-data'><WeatherData {...forecast} /></span>
+          </>
+  
+        ))}
+      </section>
+    </div>
+  );
+};
+
+
+
+export default App;
